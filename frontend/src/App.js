99434403e088f4b9cdb354e5791134a6bca72249
unknown <psy_er@ad.somansa.com>
2024-08-07 17:24:10 +0900

    feat : create navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './Navbar';
 import FileForm from './components/FileForm';
 import FileList from './components/FileList';
 import InsertData from './feat_data/InsertData';
@@ -9,36 +11,39 @@ import ReadDirectory from './feat_read_directory/ReadDirectory';
 import ReadCompressedFile from './feat_read_file/ReadCompressedFile';
 import ReadFile from './feat_read_file/ReadFile';
 import ReadRocksdbMetadata from './feat_read_rocksdb_metadata/ReadRocksdbMetadata';
+import RocksdbManual from './feat_introduce_rocksdb/RocksdbManual';
 
 function App() {
     return (
-        //<div className="App">
-        //    <h1>RocksDB Browser</h1>
-        //    <FileForm />
-        //    <FileList />
-        //</div>
             <Routes>
-            {/*Key-Value 데이터*/}
-            <Route path="/data/insert" element={<InsertData />} />
-            <Route path="/data/select" element={<SelectData />} />
+                
+                {/* test */}
+                <Route path="/file" element={<FileForm />} />
+                <Route path="/filelist" element={<FileList />} />
+
+                {/*Rocksdb 메인 페이지*/}
+                <Route path="/" element={<RocksdbManual />} />
+
+                {/*Key-Value 데이터*/}
+                <Route path="/data/insert" element={<InsertData />} />
+                <Route path="/data/select" element={<SelectData />} />
             
-            {/* 새로운 Rocksdb 생성 */}
-            <Route path="/new/rocksdb" element={<NewRocksdb />} />
+                {/* 새로운 Rocksdb 생성 */}
+                <Route path="/new/rocksdb" element={<NewRocksdb />} />
       
-            {/* SST 파일 업로드 후 데이터 확인하기 */}
-            <Route path="/file/compressed" element={<ReadCompressedFile />} />
-            <Route path="/file" element={<ReadFile />} />
+                {/* SST 파일 업로드 후 데이터 확인하기 */}
+                <Route path="/file/compressed" element={<ReadCompressedFile />} />
+                <Route path="/file" element={<ReadFile />} />
       
-            {/* Rocksdb Path 업로드 후 데이터 확인하기 */}
-            <Route path="/directory/compresssed" element={<ReadCompressedDirectory />} />
-            <Route path="/directory" element={<ReadDirectory />} />
+                {/* Rocksdb Path 업로드 후 데이터 확인하기 */}
+                <Route path="/directory/compresssed" element={<ReadCompressedDirectory />} />
+                <Route path="/directory" element={<ReadDirectory />} />
 
-            {/* Rocksdb Metadata */}
-            <Route path="/metadata" element={<ReadRocksdbMetadata />} />
-      
-          </Routes>
+                {/* Rocksdb Metadata */}
+                <Route path="/metadata" element={<ReadRocksdbMetadata />} />
 
-
+            </Routes>
+        
     );
 }
 
