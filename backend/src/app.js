e665e42c2bb56d2de72376a5875ab20c448aa54b
unknown <psy_er@ad.somansa.com>
2024-08-08 18:29:41 +0900

    feat : Apply facebook levelup leveldown to flush

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,16 +1,16 @@
-// Express 애플리케이션을 설정하고 미들웨어 추가
 const express = require('express');
 const bodyParser = require('body-parser');
-const rocksdbRoutes = require('./routes/rocksdbRoutes');
-const app = express();
 const cors = require('cors');
+const rocksdbRoutes = require('./routes/rocksdbRoutes');
+
+const app = express();
 
 app.use(cors()); // cors 설정
 app.use(bodyParser.json());
-app.use('/rocksdb', rocksdbRoutes);
+app.use('/rocksdb', rocksdbRoutes); // 올바른 경로로 라우터 사용
 
-// 라우트 설정
+// 추가 라우트 설정 (예: 사용자 라우트)
+// const userRoutes = require('./routes/userRoutes');
 // app.use('/api/users', userRoutes);
-
 
 module.exports = app;
