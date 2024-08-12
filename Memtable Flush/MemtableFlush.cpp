6b0b27600b8195a86c1c5145f4a6de815e843d8a
unknown <psy_er@ad.somansa.com>
2024-08-12 13:22:58 +0900

    feat : navbar settings, logo image, logo

--- Memtable Flush/MemtableFlush.cpp
+++ Memtable Flush/MemtableFlush.cpp
@@ -1,34 +1,39 @@
 #include <iostream>
 #include <rocksdb/db.h>
 #include <rocksdb/status.h>
-#include <Rpc.h>
+#include <rocksdb/options.h>
 
 using namespace std;
 using namespace rocksdb;
 
 int main() {
 
-	rocksdb::DB* db;
-	rocksdb::Options options;
-	options.create_if_missing = true;
+    rocksdb::DB* db;
+    rocksdb::Options options;
+    options.create_if_missing = true;
 
-	// Rocksdb 열기
-	rocksdb::Status status = rocksdb::DB::Open(options, "test", &db);
-	if (!status.ok()) {
-		cerr << "Unable to open RocksDB: " << status.ToString() << endl;
-		return 1;
-	}
+    // RocksDB 열기
+    rocksdb::Status status = rocksdb::DB::Open(options, "test", &db);
+    if (!status.ok()) {
+        cerr << "Unable to open RocksDB: " << status.ToString() << endl;
+        return 1;
+    }
 
-	// Memtable Flush 수행
-	status = db->Flush(rocksdb::FlushOptions());
-	if (!status.ok()) {
-		cerr << "Fail to flush memtable" << status.ToString() << endl;
-	}
-	else {
-		cout << "Success to flush memtable" << endl;
-	}
-	
-	delete db;
-	return 0;
+    // 데이터 삽입
+    status = db->Put(rocksdb::WriteOptions(), "key1", "value1");
+    if (!status.ok()) {
+        cerr << "Failed to put data: " << status.ToString() << endl;
+    }
 
+    // MemTable Flush 수행
+    status = db->Flush(rocksdb::FlushOptions());
+    if (!status.ok()) {
+        cerr << "Fail to flush memtable: " << status.ToString() << endl;
+    }
+    else {
+        cout << "Success to flush memtable" << endl;
+    }
+
+    delete db;
+    return 0;
 }
--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -1,48 +1,53 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
 import "./Navbar.css";
 import { Link, useLocation, matchPath } from "react-router-dom";
 
 const Navbar = () => {
     const location = useLocation();
-    const isOpenFileLink = matchPath({path: '/file/*'}, location.pathname);
-    const isOpenDirectoryLink = matchPath({path: '/directory/*'}, location.pathname);
-    const isNew = matchPath({path: '/new/rocksdb/*'}, location.pathname);
-    const isDetail = matchPath({path: '/metadata/*'}, location.pathname);
-    const isManual = matchPath({path: '/'}, location.pathname);
+    const isOpenFileLink = matchPath({ path: '/file/*' }, location.pathname);
+    const isOpenDirectoryLink = matchPath({ path: '/directory/*' }, location.pathname);
+    const isNew = matchPath({ path: '/new/rocksdb/*' }, location.pathname);
+    const isDetail = matchPath({ path: '/metadata/*' }, location.pathname);
+    const isManual = matchPath({ path: '/' }, location.pathname);
 
     return (
         <nav className="navbar">
-            
-            {/*파일 업로드*/}
-            <Link to ="/file" className="nav-link" style={{textDecoration: 'none', fontSize: '0.7rem'}}>
+            {/* 로고 이미지 및 텍스트 */}
+            <div className="navbar-logo">
+                <img src={`${process.env.PUBLIC_URL}/logoImage.png`} alt="Logo" className="logo-image" />
+                <span className="logo-text">RocksDB Browser</span>
+            </div>
+
+            {/* 파일 업로드 */}
+            <Link to="/file" className="nav-link" style={{ textDecoration: 'none', fontSize: '1rem' }}>
                 <div className={isOpenFileLink ? 'active' : 'inactive'}>
                     OPEN FILE
                 </div>
             </Link>
 
-            {/*디렉토리 업로드*/}
-            <Link to ="/directory" className="nav-link" style={{textDecoration: 'none', fontSize: '0.7rem'}}>
+            {/* 디렉토리 업로드 */}
+            <Link to="/directory" className="nav-link" style={{ textDecoration: 'none', fontSize: '1rem' }}>
                 <div className={isOpenDirectoryLink ? 'active' : 'inactive'}>
                     OPEN DIRECTORY
                 </div>
             </Link>
 
-            {/*새로운 Rocksdb 생성*/}
-            <Link to ="/new/rocksdb" className="nav-link" style={{textDecoration: 'none', fontSize: '0.7rem'}}>
+            {/* 새로운 Rocksdb 생성 */}
+            <Link to="/new/rocksdb" className="nav-link" style={{ textDecoration: 'none', fontSize: '1rem' }}>
                 <div className={isNew ? 'active' : 'inactive'}>
                     NEW
                 </div>
             </Link>
 
-            {/*Rocksdb 메타데이터 확인*/}
-            <Link to ="/metadata" className="nav-link" style={{textDecoration: 'none', fontSize: '0.7rem'}}>
+            {/* Rocksdb 메타데이터 확인 */}
+            <Link to="/metadata" className="nav-link" style={{ textDecoration: 'none', fontSize: '1rem' }}>
                 <div className={isDetail ? 'active' : 'inactive'}>
                     DETAIL
                 </div>
             </Link>
 
-            {/*Rocksdb 소개*/}
-            <Link to ="/manual" className="nav-link" style={{textDecoration: 'none', fontSize: '0.7rem'}}>
+            {/* Rocksdb 소개 */}
+            <Link to="/manual" className="nav-link" style={{ textDecoration: 'none', fontSize: '1rem' }}>
                 <div className={isManual ? 'active' : 'inactive'}>
                     MANUAL
                 </div>
